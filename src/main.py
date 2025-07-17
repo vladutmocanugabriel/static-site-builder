af3aeb2b2ab064a8c8bbd318be2ce40078e9071d
vladutmocanugabriel <mocanuvladutgabriel@gmail.com>
2025-07-17 22:22:25 +0300

    added the enum for text types and class for TextNode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    text_node_test =TextNode("Test", TextType.LINK, "https://www.boot.dev")
+    print(text_node_test)
+
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD_TEXT = "**Bold text**"
+    ITALIC_TEXT = "_Italic text_"
+    CODE_TEXT = "`Code text`"
+    LINK = "[anchor text](url)"
+    IMAGE = "![alt_text](url)"
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other_text_node):
+        if (self.text == other_text_node.text) and (self.text_type == other_text_node.text_type) and (self.url == other_text_node.url):
+            return True
+        else:
+            return False
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        
+
